cb97070cad55014c2cc62392e333f3b9c66d48dc
Kaiden Lee <110312105+kaidenslee@users.noreply.github.com>
2026-03-03 22:53:18 -0500

    Update Receiver.java
    used external ide to fix syntax errors, adjust universal receiver, and add comments

--- Reciever/Receiver.java
+++ Reciever/Receiver.java
@@ -1,3 +1,4 @@
+
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.DatagramPacket;
@@ -5,14 +6,13 @@ import java.net.DatagramSocket;
 import java.net.InetAddress;
 
 
-//handshake, data transfer (stop and wait, go back n), EOT 
 //if window is omitted -> stop and wait 
 //if window size is provided --> go back n
 
-
+//receiver is designed to handle handshake, data transfer (stop and wait or go back n), and EOT
 public class Receiver{
-  private static final int PACKET_SIZE = DSPacket.MAX_PACKET_SIZE;
-  private DatagramSocket socket;
+  private static final int PACKET_SIZE = DSPacket.MAX_PACKET_SIZE;//fixed packet size
+  private DatagramSocket socket;//udp socket receiver
   private int MOD = 128;
 
   private InetAddress sender_ip;
@@ -22,8 +22,8 @@ public class Receiver{
   private int RN;
 
   
-  private int windowSize; 
-  private int ackCount = 0;
+  private  int windowSize; 
+  private int ackCount = 0;//counter used by chaosEngine 
 
   public Receiver(InetAddress sender_ip, int sender_ack_port, int rcv_data_port, String output_file, int RN, int windowSize) throws IOException{
     this.sender_ip = sender_ip;
@@ -33,12 +33,15 @@ public class Receiver{
     this.windowSize = windowSize;
     this.RN = RN;
 
+    //connect socket so receiver can listen for SOT, data, or EOT
     this.socket = new DatagramSocket(this.rcv_data_port);
     }
-
+  
+  
+//for sending an ACK paket with given seq number, 
     private void sendAck(int seq) throws IOException{
       ackCount++;
-      if(ChaosEngine.shouldDrop(ackCount, RN)){
+      if(ChaosEngine.shouldDrop(ackCount, RN)){//chaosEngine decides if ACK needs to be dropped
         return;//simulate loss
       }
       DSPacket ack = new DSPacket(DSPacket.TYPE_ACK, seq, null);
@@ -47,6 +50,8 @@ public class Receiver{
       socket.send(datagram_packet);
 
     }
+    
+    
   //handshake. recieves SOT (type 0, seq 0), sends ACK (type 2, seq 0)
   private void handshake()throws IOException{
     while(true){
@@ -64,6 +69,8 @@ public class Receiver{
     }
 
   }
+  
+  //stop and wait receiver for maintaining expectedseq, write payload, ack seq, and increment expectedseq
   private void stop_and_wait()throws IOException{
     try(FileOutputStream fos = new FileOutputStream(output_file)){
       int expectedSeq = 1;
@@ -84,6 +91,7 @@ public class Receiver{
             expectedSeq = (expectedSeq + 1) % MOD;
           }else{
             sendAck(lastInOrder);
+          }
           }else if(type == DSPacket.TYPE_EOT){
             sendAck(seq);
             return;
@@ -97,12 +105,10 @@ public class Receiver{
     }
 
   }
-
+ 
+//go back n receiver 
+//buffers out of order packets within window and deliver in order when able
   private void go_back_n() throws IOException {
-    if (windowSize <= 0){
-      stop_and_wait();
-      return;
-  }
         if(windowSize > MOD){
           windowSize = MOD;
         }
@@ -125,12 +131,12 @@ public class Receiver{
                 if(!received[seq]){
                   buffer[seq] = packet;
                   received[seq] = true;
-                }else{
-                  //print to cl if going to
+                }
+                }else {
+                	//outside window, ignore
                 }
 
-              }
-                boolean moved = false; 
+              
                 while(received[expectedSeq]){
                   DSPacket nextUp = buffer[expectedSeq];
                   fos.write(nextUp.getPayload());
@@ -139,35 +145,36 @@ public class Receiver{
 
                   lastDelivered = expectedSeq;
                   expectedSeq = (expectedSeq + 1) % MOD;
-                  moved = true;
                 }
+                
                 sendAck(lastDelivered);
-                if(type == DSPacket.TYPE_EOT){
+                
+            } else if(type == DSPacket.TYPE_EOT){
                   sendAck(seq);
 
                   while (received[expectedSeq]){
                     DSPacket nextUp = buffer[expectedSeq];
                     fos.write(nextUp.getPayload());
-                    buffer[expectedSeq] = false;
+                    buffer[expectedSeq] = null;
+                    received[expectedSeq] = false;
                     lastDelivered = expectedSeq;
                     expectedSeq = (expectedSeq + 1) % MOD;
                   }
                   return;
+                } else {
+                	//ignore others
                 }
               }
-            }
+           
 
-          }//yo fix these closing brackets ??
+          }
   
     //ensure window size N is multiple of 4, n <= total packet size. 
-    //use buffering. if within window, buffer if not received, dilver in order while possible
-    //send one cumulative ack 
-    //discard below ro above window, resend cumaltive ack
-
+  
   }
 
-
-  public void run(){
+//runs receiver via handshake, choosing between stop and wait or go back n, and closing socket
+  public void run()throws IOException{
     handshake();
     if(windowSize <= 0){
       stop_and_wait();
@@ -175,25 +182,27 @@ public class Receiver{
       go_back_n();
     }
     socket.close();
+  }
 
-
-public static void main(String[] args){
-  try{
-    InetAddress sender_ip = InetAddress.getByName(args[0];
+public static void main(String[] args) throws Exception{
+	 if (args.length < 5) {
+	    	return; 
+	    	//add error message
+	    }
+    InetAddress sender_ip = InetAddress.getByName(args[0]);
     int sender_ack_port = Integer.parseInt(args[1]);
     int rcv_data_port = Integer.parseInt(args[2]);
     String output_file = args[3];
     int RN = Integer.parseInt(args[4]);
     int windowSize = 0;
+   
     if(args.length >= 6){
       windowSize = Integer.parseInt(args[5]);
     }
+    
 
     Receiver receiver = new Receiver(sender_ip, sender_ack_port, rcv_data_port, output_file, RN, windowSize);
     receiver.run();
 
 }
-   
-        
-    
-    
+}
